49db893078e8fb4550e08a8f678e87ea2121c7e1
Samual Norman <samualtnorman@gmail.com>
2023-05-07 16:54:27 +0100

    fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { useRouteData } from "solid-start"
-import { createServerAction$, createServerData$ } from "solid-start/server"
+import { createServerAction$, createServerData$, redirect } from "solid-start/server"
 
 let count = 0
 
@@ -7,7 +7,10 @@ export const routeData = () => createServerData$(() => count)
 
 export default () => {
 	const getRouteData = useRouteData<typeof routeData>()
-	const [ _, increment ] = createServerAction$(async (form: FormData) => count++)
+	const [ _, increment ] = createServerAction$(async (form: FormData) => {
+		count++
+		return redirect("/")
+	})
 
-	return <increment.Form><input type="button" value={getRouteData()}/></increment.Form>
+	return <increment.Form><input type="submit" value={getRouteData()}/></increment.Form>
 }
